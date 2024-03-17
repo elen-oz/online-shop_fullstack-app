62495fef93b24e2067450404a683213c396165ea
elen-oz <ozerova.lenka@gmail.com>
2024-03-17 13:50:00 +0100

    feat: add routes and endpoints

--- server/index.js
+++ server/index.js
@@ -3,16 +3,17 @@ const express = require('express');
 const sequelize = require('./db.js');
 const models = require('./models/models.js');
 const cors = require('cors');
+const router = require('./routes/index.js');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'WORKING!!' });
-});
+app.use('/api', router);
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'WORKING!!' });
+// });
 
 const start = async () => {
   try {
