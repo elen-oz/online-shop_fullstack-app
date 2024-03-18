771a275e7a79255b09ffc869be742368e55705bc
elen-oz <ozerova.lenka@gmail.com>
2024-03-18 11:14:31 +0100

    feat: add checking for admin role for creating brand & device

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -6,8 +6,5 @@ const authMiddleware = require('../middleware/authMiddleware');
 router.post('/registration', userController.registration);
 router.post('/login', userController.login);
 router.get('/auth', authMiddleware, userController.check);
-// router.get('/auth', (req, res) => {
-//   res.json({ message: 'AUTH working' });
-// });
 
 module.exports = router;
